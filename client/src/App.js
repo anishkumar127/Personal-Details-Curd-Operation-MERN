cf6e2348a401b8cc3aadcc9c9bbee69e0754e549
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-18 01:03:55 +0530

    routes added. home and edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {BrowserRouter,Route, Routes} from 'react-router-dom';
+import Edit from './pages/Edit';
+import Home from './pages/Home';
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/edit/:id" element={<Edit/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
