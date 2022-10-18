bfaf79557c93282c24fab046704ec434e314d239
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-19 00:35:09 +0530

    form added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,17 +3,37 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div style={{backgroundColor:"gray"}}>
-            <h1 className="text-white text-center mt-2"> MERN CURD </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div style={{ backgroundColor: "gray" }}>
+              <h1 className="text-white text-center mt-2"> MERN CURD </h1>
+            </div>
           </div>
+          <div className="col-md-6">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputPassword1"/>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Age</label>
+                <input type="number" class="form-control" id="exampleInputPassword1"/>
+              </div>
+             
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+          </div>
+          <div className="col-md-6">Table</div>
         </div>
-        <div className="col-md-6">Form</div>
-        <div className="col-md-6">Table</div>
       </div>
-    </div>
 
     </>
   )
